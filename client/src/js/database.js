8e7299dddb041f2338403109d48da4670bbe201b
offbrandcat <128991168+offbrandcat@users.noreply.github.com>
2023-06-12 15:54:47 -0400

    editted cilent jscript

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,31 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const jatetDB = await openDB('jate',1)
+  const tx = jatetDB.transaction('jate','readwrite')
+  const store = tx.objectStore('jate')
+  const reqeust = store.put({ id: id, value: content})
+  const result = await reqeust;
+  console.log('result.value', result.value)
+  return result
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
 
+  const jatetDB = await openDB('jate',1);
+
+  const tx = jatetDB.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.get(1);
+
+  const result = await request;
+  console.log('result.value', result.value);
+  return result;
+};
 initdb();
